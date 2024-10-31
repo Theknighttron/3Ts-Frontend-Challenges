f5d0d5bea51adde4738a22c255a4bccbbfac4e32
Bioss <bios.molley@gmail.com>
2024-10-31 16:58:39 +0300

    challenge-2

--- bioto/src/App.js
+++ bioto/src/App.js
@@ -1,11 +1,8 @@
 
 function App() {
   return (
-    <div style={{ backgroundColor: 'lightblue', padding: '3px', textAlign: 'center' }}
-      className="App">
-      <h1 style={{ color: 'darkblue', fontSize: '30px' }}>
-        Hello Bioto, Welcome to React</h1>
-
+    <div>
+      <h1 className="text-2xl text-green-600 font-bold">Hello Bioto, Welcome to React</h1>
     </div>
   );
 }
